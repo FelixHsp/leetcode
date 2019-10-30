c2ef2df3faddc95dc3344bb4e7250dc22fbe6910
Felix <13115412392@163.com>
2019-10-30 11:25:20 +0800

    回文子串

---
+++ 5_回文子串.js
@@ -0,0 +1,17 @@
+var countSubstrings = function(s) {
+    var number = 0;
+    function find(s, i, j) {
+        while (i >= 0 && j <s.length && s[i] == s[j]) {
+            number++;
+            console.log(i,j)
+            i--;
+            j++;
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        find(s, i, i);
+        find(s, i, i+1);
+    }
+    // console.log(number);
+};
+countSubstrings("aba");
