10db3d6b05d6c9212c9ef3258dab71662676dde9
Felix <13115412392@163.com>
2022-04-17 21:36:07 +0800

    feat: add 844

---
+++ easy/844_比较含退格的字符串.ts
@@ -0,0 +1,32 @@
+const backspaceCompare = (s: string, t: string): boolean => {
+  const sStack = [];
+  const tStack = [];
+
+  const length = s.length >= t.length ? s.length : t.length;
+  for (let i = 0; i < length; i++) {
+    if (s[i]) {
+      if (s[i] !== '#') {
+        sStack.push(s[i]);
+      }
+      else {
+        sStack.pop();
+      }
+    }
+
+    if (t[i]) {
+      if (t[i] !== '#') {
+        tStack.push(t[i]);
+      }
+      else {
+        tStack.pop();
+      }
+    }
+  }
+
+  return sStack.join('') === tStack.join('');
+};
+
+const s = 'ab#c';
+const t = 'ad#c';
+
+console.log(backspaceCompare(s, t));
