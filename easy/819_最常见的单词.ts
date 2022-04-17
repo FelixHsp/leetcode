7cf92c6048651bad1392793c13f218977d820817
Felix <13115412392@163.com>
2022-04-17 21:10:00 +0800

    feat: add 819

---
+++ easy/819_最常见的单词.ts
@@ -0,0 +1,28 @@
+const mostCommonWord = (paragraph: string, banned: string[]): string => {
+  const list = paragraph.match(/\b\w+\b/g);
+  const map = {};
+  const bannedMap = {};
+  const currentMax = {
+    word: '',
+    num: 0
+  };
+
+  for (let i = 0; i < banned.length; i++) {
+    bannedMap[banned[i].toLowerCase()] = 1;
+  };
+
+  for (let i = 0; i < list.length; i++) {
+    const word = list[i].toLowerCase();
+    map[word] = map[word] + 1 || 1;
+    if (map[word] > currentMax.num && !bannedMap[word]) {
+      currentMax.word = word;
+      currentMax.num = map[word];
+    }
+  };
+
+  return currentMax.word;
+};
+
+const paragraph = 'Bob hit a ball, the hit BALL flew far after it was hit.';
+const banned = ['hit'];
+console.log(mostCommonWord(paragraph, banned));
