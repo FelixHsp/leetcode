4f39ab9fbd85d188ea40a5bf3678b5610bf9390f
Felix <13115412392@163.com>
2022-04-18 22:49:00 +0800

    feat: add 386

---
+++ mid/386_字典序排数.ts
@@ -0,0 +1,41 @@
+const lexicalOrder = (n: number): number[] => {
+  const array = [];
+  const dfs = (m: number) => {
+    if (m > n) {
+      return;
+    }
+    array.push(m);
+    for (let i = 0; i < 10; i++) {
+      if (m * 10 + i !== 0) {
+        dfs(m * 10 + i);
+      }
+    }
+  };
+  dfs(0);
+  array.shift();
+  
+  return array;
+};
+
+// function lexicalOrder(n: number): number[] {
+//   let index = 1;
+//   let startNumber = 1;
+//   const array: number[] = [];
+//   for (let i = 1; i <= n; i++) {
+//     const numberList = `${i}`.match(RegExp(startNumber + '(\\w)'));
+//     if (numberList) {
+//       array.splice(index + Number(numberList[1]), 0, i);
+//       if (numberList[1] === '9') {
+//         startNumber += 1;
+//         index = array.findIndex(number => number === startNumber) + 1;
+//       }
+//     }
+//     else {
+//       array.push(i);
+//     }
+//   };
+//   return array;
+// };
+
+const n = 23;
+console.log(lexicalOrder(n));
