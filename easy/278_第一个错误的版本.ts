7372afffd1806d3094fc6cf060489bf5be1580c9
Felix <13115412392@163.com>
2022-04-24 22:15:37 +0800

    feat: add 278

---
+++ easy/278_第一个错误的版本.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Felix 
+ * @File: https://leetcode-cn.com/problems/first-bad-version/
+ * @Tags: 二分、查找
+ * @Date: 2022-04-24 21:45:29 
+ * @Last Modified by: Felix
+ * @Last Modified time: 2022-04-24 22:14:15
+ */
+
+const solution = (isBadVersion: any) => {
+  return function(n: number): number {
+    let i = 1;
+    let j = n;
+
+    while (i < j) {
+      let midVersion = Math.floor((i + j) / 2);
+      if (isBadVersion(midVersion)) {
+        j = midVersion;
+      }
+      else {
+        i = midVersion + 1;
+      }
+    }
+
+    return i;
+  };
+};
+
+const isBadVersion = (n: number): boolean => {
+  if (n >= 5) {
+    return true;
+  }
+  return false
+};
+
+console.log(solution(isBadVersion)(5));
