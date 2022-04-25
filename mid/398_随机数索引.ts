01db19eac9f722eecfec6dc03e3c5df262d904b4
Felix <13115412392@163.com>
2022-04-25 21:05:18 +0800

    feat: add 398

---
+++ mid/398_随机数索引.ts
@@ -0,0 +1,30 @@
+/*
+ * @Author: Felix 
+ * @File: https://leetcode-cn.com/problems/random-pick-index/
+ * @Tags: map
+ * @Date: 2022-04-25 20:53:30 
+ * @Last Modified by: Felix
+ * @Last Modified time: 2022-04-25 21:04:57
+ */
+
+class Solution {
+  private map: Map<number, number[]>;
+
+  constructor(nums: number[]) {
+    this.map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+      if (!this.map.has(nums[i])) {
+        this.map.set(nums[i], [i]);
+      }
+      else {
+        this.map.get(nums[i])?.push(i);
+      }
+    }
+  }
+
+  pick(target: number): number {
+    const array = this.map.get(target) || [];
+    return array[Math.floor(Math.random() * array.length)];
+  }
+}
