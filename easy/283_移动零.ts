04d9feb0fdce8ad2de475c7c313ec576eca75115
Felix <13115412392@163.com>
2022-04-26 21:25:39 +0800

    feat: add 283

---
+++ easy/283_移动零.ts
@@ -0,0 +1,35 @@
+/*
+ * @Author: Felix 
+ * @File: https://leetcode-cn.com/problems/move-zeroes/
+ * @Date: 2022-04-26 21:10:10 
+ * @Last Modified by: Felix
+ * @Last Modified time: 2022-04-26 21:25:00
+ */
+
+// const moveZeroes = (nums: number[]): void => {
+//   let j = nums.length - 1;
+//   while (j >= 0) {
+//     if (nums[j] === 0) {
+//       nums.push(0);
+//       nums.splice(j, 1);
+//     }
+//     j--;
+//   }
+// };
+
+const moveZeroes = (nums: number[]): void => {
+  let fast = 0;
+  let slow = 0;
+  while (fast < nums.length) {
+    if (nums[fast] !== 0) {
+      [nums[slow], nums[fast]] = [nums[fast], nums[slow]];
+      slow++;
+    }
+    fast++;
+  }
+};
+
+const nums = [0];
+moveZeroes(nums);
+
+export default void 0;
