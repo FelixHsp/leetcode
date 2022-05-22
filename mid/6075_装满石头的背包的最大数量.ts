928be1b0258a7f7e03a1687dd129ae7555497593
Felix <13115412392@163.com>
2022-05-22 10:49:55 +0800

    feat: add 6075

---
+++ mid/6075_装满石头的背包的最大数量.ts
@@ -0,0 +1,36 @@
+/*
+ * @Author: Felix 
+ * @Date: 2022-05-22 10:38:40 
+ * @Last Modified by: Felix
+ * @Last Modified time: 2022-05-22 10:49:37
+ */
+
+const maximumBags = (capacity: number[], rocks: number[], additionalRocks: number): number => {
+  const reset: number[] = Array.from({ length: capacity.length }).map((_, index) => {
+    return capacity[index] - rocks[index];
+  }).sort((a, b) => {
+    return a - b;
+  });
+
+  let count: number = 0;
+  let cur: number = 0;
+  
+  while (cur < reset.length && additionalRocks) {
+    if (additionalRocks < reset[cur]) {
+      break;
+    }
+
+    if (reset[cur]) {
+      additionalRocks -= reset[cur];
+    }
+
+    count ++;
+    cur++;
+  }
+
+  return count;
+};
+
+console.log(maximumBags([10,2,2], [2,2,0], 9));
+
+export default void 0;
