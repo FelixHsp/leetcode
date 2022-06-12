f3f3f3b4d01caf58ca21cff2d1ef6ecf37487dd7
Felix <13115412392@163.com>
2022-06-12 15:38:06 +0800

    feat: add 455

---
+++ easy/455_分发饼干.ts
@@ -0,0 +1,34 @@
+/*
+ * @Author: Felix 
+ * @File: 贪心算法
+ * @Date: 2022-06-12 15:18:50 
+ * @Last Modified by: Felix
+ * @Last Modified time: 2022-06-12 15:37:50
+ */
+
+const findContentChildren = (g: number[], s: number[]): number => {
+  let result = 0;
+  
+  g.sort((a, b) => a - b);
+  s.sort((a, b) => a - b);
+  const gLen = g.length;
+  const sLen = s.length;
+  let i = 0;
+  let j = 0;
+
+  while (i < gLen && j < sLen) {
+    if (s[j] >= g[i]) {
+      j++;
+      i++;
+      result++;
+    }
+    else {
+      j++;
+    }
+  }
+  
+
+  return result;
+};
+
+export default void 0;
